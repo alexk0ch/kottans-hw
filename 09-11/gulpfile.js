bc198759d5e573f2f341b51be4a1710e4210dfdb
Alex Koch <as.kochetov@gmail.com>
2014-11-09 23:48:53 +0200

    added hw-4

---
+++ 09-11/gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var webserver = require('gulp-webserver');
+var plumber = require('gulp-plumber');
+var concat = require('gulp-concat');
+var autoprefix = require('gulp-autoprefixer');
+
+gulp.task('scss', function () {
+	gulp.src([
+					'./scss/main.scss'
+				])
+		.pipe( plumber() )
+		.pipe( sass({
+			sourceComments: 'normal',
+			errLogToConsole: true,
+	      onError: function (e) {
+	        console.log('sass error is ', e);
+	      }
+		}) )
+		.pipe( autoprefix() )
+		.pipe(concat('styles.css'))
+		.pipe( gulp.dest('./css') );
+});
+
+gulp.task('webserver', function() {
+  gulp.src('.')
+    .pipe(webserver({
+      host:             'localhost',
+      port:             '81',
+      livereload:       true,
+      directoryListing: false
+    }));
+});
+
+gulp.task('watch', function() {
+  gulp.watch('./scss/**/*.scss', ['scss']);
+});
+
+gulp.task( 'default', ['scss', 'webserver', 'watch'] );
